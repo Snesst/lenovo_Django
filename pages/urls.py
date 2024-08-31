4ac440cccc4c7969cf7f5161b96e1f7358b792ca
Snesst <hungyufai@gmail.com>
2024-08-31 12:37:10 +0800

    pages added

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
+# This file is newly created by you and type in all the commands below
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), #go to views.py to run the function called index there, it's named 'index'.
 ]
